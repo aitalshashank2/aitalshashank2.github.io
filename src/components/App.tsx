9b3a657ffd898289031c82ca237b7667a133e650
Shashank Aital <aitalshashank2@gmail.com>
2021-07-30 22:30:34 +0530

    Create a basic landing page

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="text-content">
+                SHASHANK AITAL
+            </div>
+        </div>
+    )
+}
+
+export default App
